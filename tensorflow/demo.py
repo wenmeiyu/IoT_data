84f3b80e55d24947bc6756e0d1fef6bb7fdbfeb1
wmy <1157515766@qq.com>
2019-01-16 10:02:47 +0800

    add a tensorflow

---
+++ tensorflow/demo.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# ! -*- coding:utf-8 -*-
+# 一个tensorflow 小栗子
+
+import tensorflow as tf
+# from tensorflow  import keras
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
